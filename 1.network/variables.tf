59c04f858d4074b4f3b4e35bf11a37692b4a3e1e
EBRAEKKE-LAP\ebraekke <ebraekke@yahoo.com>
2022-04-24 17:57:52 +0200

    added files

--- 1.network/variables.tf
+++ 1.network/variables.tf
@@ -1,56 +1,31 @@
 
 # Input variables
-variable "vcn_cidr"           { default = "10.0.0.0/16" }
-variable "subnet_cidr_offset" { default = 8 }
+variable "vcn_cidr"           { 
+    description = "cidr raneg for VNC"
+    default     = "10.0.0.0/16" 
+}
+
+variable "subnet_cidr_offset" { 
+    description = "offset or size of subnets"
+    default     = 8 
+}
 
 ###########################################################################
 # Used for naming output files and objects, incl. for ansible
 ###########################################################################
-variable "tenant_short_code"    {}
-variable "set_name"             {}
-variable "authorized_keys_path" { default = "config/authorized_keys" }
-
-###########################################################################
-# Resource counts and shape
-###########################################################################
-variable "web_count"        { default = "3" }
-variable "web_ocpus"        { default = "1" }
-variable "web_mem_per_ocpu" { default = "6" }
-
-###########################################################################
-# Details for VMs, ssh key vcn and subnet
-###########################################################################
-variable "ssh_public_key"   {}
+variable "tenant_short_code"    {
+    description = "identity of the tenant"
+}
+variable "set_name"             {
+    description = "name of set"
+}
 
 ###########################################################################
 # Details related to account/identity (provider.tf)
 ###########################################################################
-variable "region"           { default = "eu-stockholm-1"}
+variable "region"           { default = "eu-frankfurt-1"}
 variable "tenancy_ocid"     {}
 variable "compartment_ocid" {}
 variable "user_ocid"        {}
 variable "fingerprint"      {}
 variable "private_key_path" {}
-
-# Set to your pref depending on OS, assumes same OS family (i.e. opc or ubuntu) 
-variable "super_user"       { default = "opc" }
-
-variable "default_shape"    {}
-variable "web_image_ocid"   {}
-
-variable "db_shapes_map"  {
-    type = map 
-    default = {
-        "1"  = "MySQL.VM.Standard.E3.1.16GB"
-        "2"  = "MySQL.VM.Standard.E3.2.32GB"
-        "4"  = "MySQL.VM.Standard.E3.4.64GB"
-        "8"  = "MySQL.VM.Standard.E3.8.128GB"
-        "16" = "MySQL.VM.Standard.E3.16.256GB"
-        "24" = "MySQL.VM.Standard.E3.24.384GB"
-        "32" = "MySQL.VM.Standard.E3.32.512GB"
-        "48" = "MySQL.VM.Standard.E3.48.768GB"
-        "64" = "MySQL.VM.Standard.E3.64.1024GB"
-    }
-} 
-
-variable "db_cores"     { default = 4 }
---
+++ 2.database/outputs.tf
@@ -0,0 +1,13 @@
+
+###########################################################################
+# OUTPUTs
+###########################################################################
+
+output "db_private_ips" {
+  value = module.db.db_private_ips
+}
+
+output "db_ocid_list" {
+  value = module.db.db_ocid_list
+}
+
